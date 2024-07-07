0407d701d6928073f25c70a0250bf2b4ca613edd
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-07-07 12:49:10 +0330

    sub menu is added and the animation with js is set

--- js/script.js
+++ js/script.js
@@ -14,23 +14,24 @@ crossIcon.addEventListener("click", function () {
 });
 
 showMoreMenu.addEventListener("mouseenter", function (e) {
-  const ul = e.target.childNodes[5];
+  const ul = e.target.childNodes[4];
+  // console.log(e.target.childNodes);
   ul.style.animation = "showUp 0.35s 1";
   ul.style.display = "block";
 });
 
 showMoreMenu.addEventListener("mouseleave", function (e) {
-  const ul = e.target.childNodes[5];
+  const ul = e.target.childNodes[4];
   ul.style.display = "none";
 });
 
 showProductMenu.addEventListener("mouseenter", function (e) {
-  const div = e.target.childNodes[5];
+  const div = e.target.childNodes[4];
   div.style.animation = "showUp 0.35s 1";
   div.style.display = "flex";
 });
 
 showProductMenu.addEventListener("mouseleave", function (e) {
-  const div = e.target.childNodes[5];
+  const div = e.target.childNodes[4];
   div.style.display = "none";
 });
